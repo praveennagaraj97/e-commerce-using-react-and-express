c18d3c2e24c4ab3cf0301a9144eebc96f54127a6
Praveen <58110261+praveenNagaraj97-au7@users.noreply.github.com>
2020-08-15 18:43:38 +0530

    UserAuth Response Header Change

--- server/handlers/userHandler.js
+++ server/handlers/userHandler.js
@@ -19,11 +19,11 @@ export const signInHandler = (ModelName, responseMessage) =>
     const user = await ModelName.findOne({ email }).select("+password");
 
     // Check If User Exists
-    if (!user) return next(new AppError(`No user Found With ${email}`, 404));
+    if (!user) return next(new AppError(`No user Found With ${email}`, 200));
 
     // Check Whether Password matches.
     if (!(await user.comparePassword(password, user.password)))
-      return next(new AppError("Entered Password is Wrong", 401));
+      return next(new AppError("Entered Password is Wrong", 200));
 
     // If password is correct create New Token And Give Access.
     const token = await generateJWToken({ id: user._id });
