33f600bb3f1d158a89a00f52c39dea1c6df96272
Praveen <58110261+praveenNagaraj97-au7@users.noreply.github.com>
2020-09-09 23:48:46 +0530

    Persist redux store on referesh

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,16 +1,35 @@
 import { compose, createStore, applyMiddleware } from "redux";
-// import reduxLogger from "redux-logger";
-import reduxThunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import createReduxSagaMiddleware from "redux-saga";
 
 import reducers from "../reducers";
 import rootSaga from "../sagas";
-
 import { USER_AUTH_TYPES, WEBSITE_LOAD } from "../constants";
+
+import createEncryptor from "redux-persist-transform-encrypt";
 
 const {
   USER_STATUS: { LOAD_ACCREDITATION },
 } = USER_AUTH_TYPES;
+
+const encryptor = createEncryptor({
+  secretKey: "my-super-secret-key",
+  onError: function (error) {
+    // Handle the error.
+  },
+});
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  blacklist: ["userAuthorization"],
+
+  transforms: [encryptor],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const sagaMiddleware = createReduxSagaMiddleware();
 
@@ -21,12 +40,14 @@ if (process.env.NODE_ENV === "development") {
   composeEnhancer = compose;
 }
 
-const middlewares = [sagaMiddleware, reduxThunk];
+const middlewares = [sagaMiddleware];
 
 const store = createStore(
-  reducers,
+  persistedReducer,
   composeEnhancer(applyMiddleware(...middlewares))
 );
+
+export const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
