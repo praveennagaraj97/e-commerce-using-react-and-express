d78fa8210a2203ada228ffe8d5c180b852e40917
Praveen <58110261+praveenNagaraj97-au7@users.noreply.github.com>
2020-09-15 23:34:45 +0530

    product detail saga worker

--- client/src/sagas/ProductsSagas/productViewSagas.js
+++ client/src/sagas/ProductsSagas/productViewSagas.js
@@ -1,12 +1,26 @@
+/**
+ * Product Detail Saga.
+ * @summary - take Product Id to get the Product details from api!
+ * @description - This saga will fetch product details along with similar products from api only once!
+ *                If the user/client requests for same product detail, the detail will not be fetched from API.
+ *                Instead the details will be fetched from store !!!
+ *                To avoid performance-lag once the store requests reaches 100 requests the last item will and re-pushed!!!
+ */
+
 import { takeLatest, select, call, put, delay } from "redux-saga/effects";
 
 import { PRODUCT_TYPES } from "../../constants";
 
+// history is used here as withRouter is not supported outside JSX component!
+import history from "../../history";
+
+// API Endpoints!
 import {
   getProductDetailEndPoint,
   getProductsDetailsInCartEndPoint as getProductDetailStoredinarray,
 } from "../../api";
-import history from "../../history";
+
+// Actions are dispatched using saga put!
 import {
   getProductDetail,
   globalFailureMessenger,
@@ -15,10 +29,14 @@ import {
 
 const { LOAD_VIEW_PRODUCT_DETAIL } = PRODUCT_TYPES;
 
+// To get the Product Detail
+// When view action is dispatched the action takes product type which containes category and product Id.
 const getViewDetailsDataFromStore = ({ productDetail }) => productDetail;
 
+// To get Stores Product Details Requests
 const getStoredRequestsFromStore = ({ requests }) => ({ requests });
 
+// Saga-Worker
 function* handleLoadProductViewWorker() {
   const { productType } = yield select(getViewDetailsDataFromStore);
 
@@ -49,16 +67,13 @@ function* handleLoadProductViewWorker() {
 
       data.detail.productFullDetails[0].productId = response.data.details;
     }
-
     yield put(
       reOccuringProductDetailRequests({
         id: [productType.productId],
         data: data,
       })
     );
-
     yield put(getProductDetail(data.detail));
-
     yield history.push(`/${productType.productCategory}/detail`);
   } catch (err) {
     yield put(
@@ -70,6 +85,7 @@ function* handleLoadProductViewWorker() {
   }
 }
 
+// Saga-Watcher
 export function* loadProductViewWatcher() {
   yield takeLatest(LOAD_VIEW_PRODUCT_DETAIL, handleLoadProductViewWorker);
 }
