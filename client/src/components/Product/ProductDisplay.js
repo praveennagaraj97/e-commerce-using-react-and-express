0fd2df5d30ee161e45b65d4b710ea6295a4a39c0
Praveen <58110261+praveenNagaraj97-au7@users.noreply.github.com>
2020-09-18 16:06:06 +0530

    Trail version of Average Rating Implement

--- client/src/components/Product/ProductDisplay.js
+++ client/src/components/Product/ProductDisplay.js
@@ -43,7 +43,19 @@ const ProductDisplay = ({
   };
 
   const reviewStarRender = (review) => {
-    return <ShowRating value={review} />;
+    if (review.length === 0) {
+      return <ShowRating value={0} />;
+    }
+
+    /**
+     * @desc Not-efficent!!
+     **/
+    let averageNumbers = 0;
+    review.forEach((each) => {
+      averageNumbers = averageNumbers + each.averageReview;
+    });
+
+    return <ShowRating value={averageNumbers / review.length} />;
   };
 
   return (
@@ -80,7 +92,13 @@ const ProductDisplay = ({
             {products.length > 0 ? (
               <Fragment>
                 {products.map(
-                  ({ _id, productCoverImage, productName, productPrice }) => {
+                  ({
+                    _id,
+                    productCoverImage,
+                    productName,
+                    productPrice,
+                    averageReview,
+                  }) => {
                     return (
                       <div key={_id} className='product-card'>
                         <div className='product-card-image_container'>
@@ -94,7 +112,7 @@ const ProductDisplay = ({
                         <div className='product-card-contents_container'>
                           <h1 className='product-card__title'>{productName}</h1>
                           <div className='product-review-container'>
-                            {reviewStarRender(4)}
+                            {reviewStarRender(averageReview)}
                           </div>
                           <p className='product-card__price'>₹{productPrice}</p>
                           <div className='product-card__view__cart_btn_option'>
