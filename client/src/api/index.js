ee8b24334b4d1dd0ce70019d6a7828d41ac7ed16
Praveen <58110261+praveenNagaraj97-au7@users.noreply.github.com>
2020-09-22 21:07:29 +0530

    added Heroku Link

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,13 @@
 import Axios from "axios";
-//
+/**
+ * @param{G-App-Engine} : "https://lexa-api.uc.r.appspot.com/api/v1"
+ * @param Heroku-Engine : "https://lexa-api-v1.herokuapp.com/api/v1"
+ */
+
 export const apiBaseEndpoint = Axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://lexa-api.uc.r.appspot.com/api/v1"
+      ? `https://lexa-api-v1.herokuapp.com/api/v1`
       : `http://localhost:8080/api/v1`,
 });
 
