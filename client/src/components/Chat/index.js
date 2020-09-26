bb51affd5d571dda938244363240f5d82fb38934
Praveen <58110261+praveenNagaraj97-au7@users.noreply.github.com>
2020-09-26 23:32:59 +0530

    Chat State

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -1,69 +1,82 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 
 import "../../styles/chat.scss";
 
-const recieved = [
-  {
-    message:
-      "yeah, they do early flights cause they connect with big airports. they wanna get u to your connection",
-    at: "51 min",
-  },
-];
-
-const sent = [
-  {
-    message:
-      "yeah, they do early flights cause they connect with big airports. they wanna get u to your connection",
-    at: "51 min",
-  },
-];
-
 const Chat = () => {
+  const [sentMessage, setSentMessage] = useState("");
+
+  const [sent, setSent] = useState([
+    {
+      message:
+        "yeah, they do early flights cause they connect with big airports. they wanna get u to your connection",
+      at: "51 min",
+    },
+  ]);
+
+  const [recieved, setRecieved] = useState([
+    {
+      message:
+        "yeah, they do early flights cause they connect with big airports. they wanna get u to your connection",
+      at: "51 min",
+    },
+  ]);
+
   return (
     <div className='chat-container'>
       <div className='chats'>
         {/* Message container */}
         <ol className='messages__container'>
           {/* Recieved */}
-          <li className='recieved'>
-            <div className='pointer'></div>
-            <div className='messages'>
-              {recieved.map(({ message, at }, index) => {
-                return (
-                  <Fragment key={index}>
-                    <p>{message}</p>
-                    <time>{at}</time>
-                  </Fragment>
-                );
-              })}
-            </div>
-          </li>
+          {recieved.map(({ message, at }, index) => {
+            return (
+              <li className='recieved' key={index}>
+                <div className='pointer'></div>
+                <div className='messages'>
+                  <p>{message}</p>
+                  <time>{at}</time>
+                </div>
+              </li>
+            );
+          })}
 
           {/* Sent */}
-          <li className='self'>
-            <div className='pointer'></div>
-            <div className='messages'>
-              {sent.map(({ message, at }, index) => {
-                return (
-                  <Fragment key={index}>
-                    <p>{message}</p>
-                    <time>{at}</time>
-                  </Fragment>
-                );
-              })}
-            </div>
-          </li>
+          {sent.map(({ message, at }, index) => {
+            return (
+              <li key={index} className='self'>
+                <div className='pointer'></div>
+                <div className='messages'>
+                  <p>{message}</p>
+                  <time>{at}</time>
+                </div>
+              </li>
+            );
+          })}
         </ol>
+        {/* Char Input */}
+        <div className='chat-input'>
+          <input
+            type='text'
+            onChange={(ev) => setSentMessage(ev.target.value)}
+            value={sentMessage}
+            placeholder='type your message'
+          />
+          <img
+            onClick={() => {
+              setSent([...sent, { message: sentMessage, at: "now" }]);
+              setSentMessage("");
+            }}
+            src='https://img.icons8.com/fluent/144/000000/filled-sent.png'
+            alt='send'
+          />
+        </div>
       </div>
-
-      {/* Char Input */}
-      <div className='chat-input'>
-        <input type='text' placeholder='type your message' />
-        <img
-          src='https://img.icons8.com/fluent/144/000000/filled-sent.png'
-          alt='send'
-        />
-      </div>
+      <button
+        onClick={() => {
+          setRecieved([...recieved, { message: sentMessage, at: "now" }]);
+          setSentMessage("");
+        }}>
+        Temp recieve
+      </button>
     </div>
   );
 };
